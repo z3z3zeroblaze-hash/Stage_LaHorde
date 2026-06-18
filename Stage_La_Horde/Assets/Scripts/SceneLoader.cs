5d06a0e9c4eb1692094d307885bdccfcc70ef7fe
JLK-cell <jonas.pouget@gmail.com>
2026-06-18 13:51:19 +0200

    V17

---
+++ Stage_La_Horde/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadIntro2()
+    {
+        SceneManager.LoadScene("Intro2");
+    }
+    public void LoadIntro3()
+    {
+        SceneManager.LoadScene("Intro3");
+    }
+
+    public void LoadIntro4()
+    {
+        SceneManager.LoadScene("Intro4");
+    }
+
+    public void LoadIntro5()
+    {
+        SceneManager.LoadScene("Intro5");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
