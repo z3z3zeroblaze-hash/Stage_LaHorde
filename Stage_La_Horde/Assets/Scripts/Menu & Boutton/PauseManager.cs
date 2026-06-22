2031d8385281c5f6d927f23aef6caaa024ff0257
gabrielaujoux-ops <z3z3zeroblaze@gmail.com>
2026-06-22 10:44:38 +0200

    V36

--- Stage_La_Horde/Assets/Scripts/Menu & Boutton/PauseManager.cs
+++ Stage_La_Horde/Assets/Scripts/Menu & Boutton/PauseManager.cs
@@ -4,12 +4,14 @@ using UnityEngine.InputSystem;
 
 public class PauseManager : MonoBehaviour
 {
+    public string scenePause = "Settings";
+
     void Update()
     {
         if (Keyboard.current != null &&
             Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            SceneManager.LoadScene("Settings");
+            SceneManager.LoadScene(scenePause);
         }
     }
 }
